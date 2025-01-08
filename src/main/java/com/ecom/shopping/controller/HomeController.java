63506c423c009ff51757009045f0b202d1a86189
Rohan Yadav <itsrohanyadav20@gmail.com>
2025-01-08 12:04:19 +0530

    Initial commit

---
+++ src/main/java/com/ecom/shopping/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.ecom.shopping.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+
+public class HomeController {
+    public String index()
+    {
+        return "index";
+
+    }
+
+}
